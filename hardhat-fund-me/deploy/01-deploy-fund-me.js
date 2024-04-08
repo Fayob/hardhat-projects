42150452a26beeec790f007df52683fbd857efa5
fayob <adedejifavour6@gmail.com>
2024-04-08 17:01:35 +0100

    Add deploy file to deploy our contract

---
+++ hardhat-fund-me/deploy/01-deploy-fund-me.js
@@ -0,0 +1,28 @@
+const { network } = require("hardhat");
+const {
+  networkConfig,
+  developmentChains,
+} = require("../helper-hardhat-config");
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+  const { deploy, log } = deployments;
+  const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+
+  // const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"]
+  let ethUsdPriceFeedAddress;
+  if (developmentChains.includes(network.name)) {
+    const ethUsdAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUsdAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
+
+  const args = [ethUsdPriceFeedAddress]
+  const fundMe = await deploy("FundMe", {
+    from: deployer,
+    args: args, // put price feed address
+    log: true,
+    waitConfirmations: network.config.blockConfirmations || 1,
+  });
+};
