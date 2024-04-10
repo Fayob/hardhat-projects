f4514c49ef92bbd2bfb15dc247afdb0c656f8b7c
fayob <adedejifavour6@gmail.com>
2024-04-10 16:24:23 +0100

    Test fund

--- hardhat-fund-me/test/unit/FundMe.test.js
+++ hardhat-fund-me/test/unit/FundMe.test.js
@@ -1,10 +1,12 @@
 const { getNamedAccounts, deployments, ethers } = require("hardhat");
-const { assert } = require("chai");
+const { assert, expect } = require("chai");
 
 describe("FundMe", async function () {
   let deployer;
   let fundMe;
   let mockV3Aggregator;
+  const sendLowValue = ethers.parseEther("0.001");
+  const sendHighValue = ethers.parseEther("1");
   beforeEach(async function () {
     // deploy FundMe Contract
     deployer = (await getNamedAccounts()).deployer;
@@ -21,5 +23,24 @@ describe("FundMe", async function () {
 
       assert.equal(responseRunnerAddress, mockV3AggregatorRunnerAddress);
     });
+  });
+
+  describe("fund", async function () {
+    it("Fails if you don't send enough Eth", async function () {
+      await fundMe.fund({ value: sendLowValue });
+        await expect(fundMe.fund()).to.be.reverted
+    });
+  });
+
+  describe("update the amount funded data structure", async function () {
+    await fundMe.fund({ value: sendHighValue });
+    const response = await fundMe.getAddresstoAmountFunded(deployer);
+    assert.equal(response.toString(), sendHighValue.toString());
+  });
+
+  describe("Adds funder to array of getFunder", async function () {
+    await fundMe.fund({ value: sendHighValue });
+    const funder = await fundMe.getFunder(0);
+    assert.equal(funder, deployer);
   });
 });
