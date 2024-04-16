648a1db1212ec85ddb7d740c1bb0a1178d61dffd
fayob <adedejifavour6@gmail.com>
2024-04-16 16:40:56 +0100

    Add more functionality and listen fot transaction to be mined functionality

--- hardhat-fund-me-frontend/index.js
+++ hardhat-fund-me-frontend/index.js
@@ -1,10 +1,14 @@
 import { ethers } from "./ethers.min.js";
-import { contractAddress } from "./constants.js";
+import { abi, contractAddress } from "./constants.js";
 
 const connectbtn = document.getElementById("connectbtn");
+const fundbtn = document.getElementById("fundbtn");
 const balancebtn = document.getElementById("balancebtn");
+const withdrawbtn = document.getElementById("withdrawbtn");
 connectbtn.onclick = connect;
+fundbtn.onclick = fund;
 balancebtn.onclick = getBalance
+withdrawbtn.onclick = withdraw
 
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
@@ -30,3 +34,56 @@ async function getBalance() {
         console.log("please connect to a provider first");
     }
  }
+// fund
+async function fund() {
+  const ethAmount = document.getElementById("ethAmount").value;
+  console.log(`Account funded with ${ethAmount}`);
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, abi, signer);
+    try {
+      const transactionResponse = await contract.fund({
+        value: ethers.parseEther(ethAmount),
+      });
+
+      // listen for transaction to be mined
+      await listenForTxToBeMined(transactionResponse, provider)
+      console.log("Done!");
+      
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
+
+// withdraw
+async function withdraw() {
+    const ethAmount = 2
+  console.log(`Withdrawing  ${ethAmount}`);
+  if (typeof window.ethereum !== "undefined") {
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    const signer = provider.getSigner()
+    const contract = new ethers.Contract(contractAddress, abi, signer)
+    try {
+        const transactionResponse = contract.withdraw()
+        await listenForTxToBeMined(transactionResponse, provider)
+    } catch (error) {
+        console.log(error);
+    }
+  }
+}
+
+function listenForTxToBeMined(transactionResponse, provider) {
+  console.log(`Mining ${transactionResponse.hash}`);
+  // listen for this transacrion to finish
+  return new Promise((resolve, reject) => {
+    provider.once(transactionResponse.hash, (transactionRecieipt) => {
+      console.log(
+        `Completed with ${transactionRecieipt.confirmations} confirmations`
+      );
+      resolve();
+    });
+  });
+}
+
