1ea8ea441de5ce2c1775c4add5c6b02577af4537
fayob <adedejifavour6@gmail.com>
2024-04-16 16:39:16 +0100

    Add get balance functionality and connect with html file

--- hardhat-fund-me-frontend/index.js
+++ hardhat-fund-me-frontend/index.js
@@ -1,3 +1,11 @@
+import { ethers } from "./ethers.min.js";
+import { contractAddress } from "./constants.js";
+
+const connectbtn = document.getElementById("connectbtn");
+const balancebtn = document.getElementById("balancebtn");
+connectbtn.onclick = connect;
+balancebtn.onclick = getBalance
+
 async function connect() {
   if (typeof window.ethereum !== "undefined") {
     console.log("Ethereum network can be found in this browser");
@@ -11,3 +19,14 @@ async function connect() {
       "Please install metamask on your device!";
   }
 }
+
+async function getBalance() {
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.BrowserProvider(window.ethereum)
+        const balance = await provider.getBalance(contractAddress)
+        // console.log(balance);
+        console.log(ethers.parseEther(balance.toString()));
+    } else {
+        console.log("please connect to a provider first");
+    }
+ }
