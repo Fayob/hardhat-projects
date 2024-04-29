e13f7cdff5a9c6b33e20dd349da371083c45be1c
fayob <adedejifavour6@gmail.com>
2024-04-29 15:24:57 +0100

    Test enterRaffle function

--- hardhat-lottery/test/unit/Raffle.test.js
+++ hardhat-lottery/test/unit/Raffle.test.js
@@ -1,6 +1,6 @@
 const { network, getNamedAccounts, deployments, ethers } = require("hardhat")
 const { developmentChains, networkConfig } = require("../../helper-hardhat-config")
-const { assert } = require("chai")
+const { assert, expect } = require("chai")
 
 !developmentChains.includes(network.name) 
     ? describe.skip 
@@ -22,6 +22,30 @@ const { assert } = require("chai")
                 const raffleState = await raffle.getRaffleState()
                 assert.equal(raffleState.toString(), "0")
                 assert.equal(interval.toString(), networkConfig[chainId]["interval"])
+            })
+        })
+
+        describe("enterRaffle", function () {
+            it("reverts when you don't pay enough", async function () {
+                await expect(raffle.enterRaffle()).to.be.revertedWith("Raffle__NotEnoughEthEntered()")
+            })
+            it("records players when they enter", async function () {
+                await raffle.enterRaffle({ value: raffleEntranceFee })
+                const playerFromContract = await raffle.getPlayer(0)
+                assert.equal(playerFromContract, deployer)
+            })
+            //Testing event
+            it("emits event on enter", async function () {
+                await expect(raffle.enterRaffle({value: raffleEntranceFee})).to.emit(raffle, "RaffleEnter")
+            })
+
+            it("doesn't allow entrance when the raffle is calculating", async function () {
+                await raffle.enterRaffle({ value: raffleEntranceFee })
+                await network.provider.send("evm_increaseTime", [interval.toNumber() + 1])
+                await network.provider.send("evm_mine", [])
+                // We pretend to be a Chainlink Keeper
+                await raffle.performUpkeep([])
+                await expect(raffle.enterRaffle({ value: raffleEntranceFee })).to.be.revertedWith("Raffle__NotOpen")
             })
         })
     })
