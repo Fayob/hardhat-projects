d05b17e3727e49f48a1e3e7618a1d032c811e41d
fayob <adedejifavour6@gmail.com>
2024-05-15 15:59:40 +0100

    Wrap component with notification

--- hardhat-lottery-frontend-nextjs/app/page.js
+++ hardhat-lottery-frontend-nextjs/app/page.js
@@ -1,15 +1,18 @@
 'use client'
 
 import { MoralisProvider } from "react-moralis";
+import { NotificationProvider } from "web3uikit";
 import Header from "@/components/Header";
 // import ManualHeader from "@/components/ManualHeader";
 
 export default function Home() {
   return (
     <MoralisProvider initializeOnMount={false}>
+      <NotificationProvider>
         <h1>Home Page</h1>
         <Header />
         {/* <ManualHeader /> */}
+      </NotificationProvider>
     </MoralisProvider>
   );
 }
